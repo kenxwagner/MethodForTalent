2a68f753dc5563823d9b2d9ff781a643bf9107b2
Ken Wagner <ken_xwagner@hotmail.com>
2018-12-02 23:32:47 -0800

    Update

--- teaching/loops.java
+++ teaching/loops.java
@@ -1,12 +1,6 @@
-//Create a triangle of asterisks
-for i in range(1, n +1):
-	for j in range(i):
-		print'*',
-	print
-	
-	//Java
-	//C#
-	//Python
-	//Ruby
-	//JavaScript
-	
+public class NestedLoopTest {
+    
+    public static void main(String[] args) {
+      
+    }
+}
