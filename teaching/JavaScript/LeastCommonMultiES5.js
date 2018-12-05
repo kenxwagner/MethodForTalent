8706e0ddee1e0526fa01eefa972a070d16350870
Ken Wagner <ken_xwagner@hotmail.com>
2018-12-04 23:34:57 -0800

    Update

--- teaching/JavaScript/LeastCommonMultiES5.js
+++ teaching/JavaScript/LeastCommonMultiES5.js
@@ -1,3 +1,9 @@
 function LeastComMult(A) {
 	var n = A.length, a = Math.abs(A{0});
+	for (var = i = 1; i < n; i++)
+	{ var b = Math.abs(A[i]), c = a;
+		while (a && b){ a > b ? a %= b : %= a; }
+		a = Math.abs(c*A[i])/(a+b);
+		}
+		return a;
 }
